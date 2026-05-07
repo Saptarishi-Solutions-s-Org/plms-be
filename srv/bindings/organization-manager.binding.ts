7c212ba89078b5aae9d6fbafbdad428d1b134d1c
srijasaithya01 <srijasaithya@gmail.com>
2026-05-07 16:59:54 +0530

    manager dashboard binding

--- srv/bindings/organization-manager.binding.ts
+++ srv/bindings/organization-manager.binding.ts
@@ -2,7 +2,7 @@ import cds from "@sap/cds";
 import { withAuth } from "../lib/withAuth";
 import { managerDashboardHandler } from "../handlers/organization-manager/getmanagerstats";
 import { leadStatusOverviewHandler } from "../handlers/organization-manager/getleadstatusoverview";
-import { executivePerformanceHandler } from "../handlers/organization-manager/getexecutiveperformance";
+
 export const bindManagerDashboard = () => {
   const service = cds.services["ManagerDashboardService"];
   if (!service) return;
@@ -17,12 +17,6 @@ export const bindManagerDashboard = () => {
   service.on(
     "getLeadStatusOverview",
     withAuth(leadStatusOverviewHandler, {
-      modules: { lead: ["view"] },
-    }),
-  );
-  service.on(
-    "getExecutivePerformance",
-    withAuth(executivePerformanceHandler, {
       modules: { lead: ["view"] },
     }),
   );
